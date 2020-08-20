ca726b6e715121ee55af02570695cb3df76226bc
Hassan Abbas <habbas375@gmail.com>
2020-08-20 12:38:53 -0500

    Added color options and improved layout

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let colorMode = "black";
+let colorMode = "normal";
+let color = "#000000";
+let shading = false;
 const board = document.getElementById("board");
+const normalButton = document.getElementById("normal-btn");
+const randomButton = document.getElementById("random-btn");
 const shadingButton = document.getElementById("shading-btn");
+const colorPicker = document.getElementById("colorPicker");
+document.getElementById("clear-btn").addEventListener("click", clear);
+document.getElementById("reset-btn").addEventListener("click", reset);
 initialize();
 
 function createBoard(gridSize){
@@ -12,8 +19,8 @@ function createBoard(gridSize){
         cell.className = "cell";
         cell.style.height = `${cellSize}px`;
         cell.style.width = `${cellSize}px`;
-        cell.addEventListener("mouseover", function(e) {
-            color(e, board);
+        cell.addEventListener("mouseenter", function(e) {
+            colorCell(e);
         })
 
         board.appendChild(cell);
@@ -21,24 +28,28 @@ function createBoard(gridSize){
 }
 
 function initialize() {
-
-    document.getElementById("black-btn").addEventListener("click", function() {
-        colorMode = "black";
-        shadingButton.style.visibility = "visible";
+    normalButton.addEventListener("click", function(e) {
+        colorMode = "normal";
+        normalButton.className = "selected";
+        randomButton.className = "";
     });
 
     shadingButton.addEventListener("click", function() {
-        colorMode = "shading";
+        shading = !shading;
+        if(shading){
+            shadingButton.className = "selected";
+        } else {
+            shadingButton.className = "";
+        }
     })
 
-    document.getElementById("random-btn").addEventListener("click", function(){
+    randomButton.addEventListener("click", function(){
         colorMode = "random";
-        shadingButton.style.visibility = "hidden";
+        normalButton.className = "";
+        randomButton.className = "selected";
     })
 
-    document.getElementById("reset-btn").addEventListener("click", function(){
-        reset();
-    })
+    colorPicker.addEventListener("slChange", changeColor)
 
     createBoard(16);
 }
@@ -49,23 +60,34 @@ function reset() {
     createBoard(size);
 }
 
+function clear(){
+    cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.background = "none";
+        cell.style.opacity = 0;
+    });
+}
+
+function changeColor(e) {
+    color = e.target.value;
+}
+
 function randomRGBValue(){
     return Math.ceil(Math.random() * 255);
 }
 
-function color(e){
+function colorCell(e){
     let cell = e.target;
 
-    if(colorMode == "random") {
-        cell.style.backgroundColor = `rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
-        cell.style.opacity = "1";
-        return;
-    }
-
-    cell.style.backgroundColor = "black";
-    if(colorMode == "shading"){
+    if(shading){
         cell.style.opacity = `${Number(cell.style.opacity) + 0.1}`;
     } else {
         cell.style.opacity = "1";
+    }
+
+    if(colorMode == "normal") {
+        cell.style.backgroundColor = color;
+    } else {
+        cell.style.backgroundColor = `rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
     }
 }
